4d744b679334b295f22bf434a342ce7f67b3a148
liba6 <lginsburg@gmail.com>
2023-08-06 22:13:24 +0200

    Add styles

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,31 @@
-runApp();
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 93, 66, 156),
+                Color.fromARGB(255, 45, 7, 98)
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              'Hello Flutter!',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 28,
+              ),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
